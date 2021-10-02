34ac89b04b37ec6535255537f55b219545b833b2
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-10-02 18:08:54 +0100

    added thr Particle class

---
+++ Particules System/Particle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Particules_System
+{
+    public class Particle
+    {
+        public double size { get; set; }
+        public TimeSpan life { get; set; }
+        public Brush colore { get; set; }
+        public Point position { get; set; }
+        public double speed { get; set; }
+    }
+}
